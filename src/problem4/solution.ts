647ae0062a740f7d01acceade85cf5346adc80f5
hoaxduong <hoadx.fu@gmail.com>
2025-10-02 15:13:50 +0700

    feat: solved problem4

--- src/problem4/solution.ts
+++ src/problem4/solution.ts
@@ -1,13 +1,16 @@
 function sum_to_n_a(n: number): number {
+  // Complexity: O(1)
 	return n * (n + 1) / 2;
 }
 
 function sum_to_n_b(n: number): number {
+  // Complexity: O(n)
   if (n <= 0) return 0;
   return n + sum_to_n_b(n - 1);
 }
 
 function sum_to_n_c(n: number): number {
+  // Complexity: O(n)
   let sum = 0;
   for (let i = 1; i <= n; i++) {
     sum += i;
